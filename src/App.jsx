e4e46ce786ab301839571ffc9810e25aa3928327
Vagner <vagnerlucion@gmail.com>
2022-04-28 11:48:01 -0300

    fixes on style

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import DropDown from './components/dropdown';
 import Item from './components/item';
+import './App.css'
 
 function App() {
   return (
-    <div style={{margin: "0 20%"}}>
+    <div className='App' style={{margin: "0 20%"}}>
       <DropDown iconStyle={{width: "32", height: "32"}} position="right">
         <Item>Rename</Item>
         <Item>Delete</Item>
         <Item>Share</Item>
-        <Item><button>Click here!</button></Item>
        </DropDown>
     </div>
   );
